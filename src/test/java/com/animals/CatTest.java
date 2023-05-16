ce603c4db041196580cdc064418c3b975cf9bf6e
Sergei Kislenko <funclock.kislenko@yandex.ru>
2023-05-16 23:17:21 +0300

    final project

--- src/test/java/com/animals/CatTest.java
+++ src/test/java/com/animals/CatTest.java
@@ -1,2 +1,29 @@
+package com.animals;
+
+import com.example.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundReturnsMeow() {
+        Cat cat = new Cat(feline);
+        Assert.assertEquals("getSound() должен вернуть\"Мяу\" ", "Мяу", cat.getSound());
+    }
+
+    @Test
+    public void getFoodPredatorEatMeatCalled() throws Exception {
+        Cat cat = new Cat(feline);
+        cat.getFood();
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
+    }
 }
--- src/test/java/com/animals/LionTest.java
+++ src/test/java/com/animals/LionTest.java
@@ -1,5 +1,46 @@
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+package com.animals;
+
+import com.example.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
+    @Mock
+    Feline feline;
+
+    @Test(expected = Exception.class)
+    public void constructorNoSexThrowsException() throws Exception {
+        Lion lion = new Lion("No sex", feline);
+    }
+
+    @Test
+    public void getKittensFelineGetKittensCalled() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        lion.getKittens();
+        Mockito.verify(feline).getKittens();
+    }
+
+    @Test
+    public void doesHaveManeMaleReturnsTrue() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Assert.assertTrue("Самец doesHaveMane() должен вернуть true", lion.doesHaveMane());
+    }
+
+    @Test
+    public void doesHaveManeFemaleReturnsFalse() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        Assert.assertFalse("Самка doesHaveMane() должен вернуть false", lion.doesHaveMane());
+    }
+
+    @Test
+    public void getFoodFelineGetFoodCalledWithPredator() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        lion.getFood();
+        Mockito.verify(feline).getFood("Хищник");
+    }
 }
